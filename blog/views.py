f7a78e0c3185fdadcaf87dfebbd38f5c6e2ffe5a
Kazeem Omoloja <omolojakazeem@gmail.com>
2021-09-09 21:45:01 +0100

    Initial Commit

---
+++ blog/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
+
+def post_list(request):
+    posts = Post.published.all()
+    context = {'posts': posts}
+    return render(request, 'blog/post/list.html', context=context)
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month,
+                             publish__day=day)
+    context = {'post': post}
+    return render(request, 'blog/post/detail.html', context=context)
